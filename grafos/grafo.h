e08e9589696a7079a9ad123ae424e9a895a1b8f2
rodrigoacosta <rodrigoacosta.pelotas@gmail.com>
2013-02-23 23:48:42 -0300

    leitura

--- grafos/grafo.h
+++ grafos/grafo.h
@@ -6,6 +6,20 @@
 #ifndef GRAFO_H_
 #define GRAFO_H_
 
+typedef char info; 
+
+typedef struct matriz{
+	int peso;                                                           /*peso nao vale para as posicoes grafo[0][x] e grafo[x][0] , pois eh onde ficara o nome*/
+	int conexo;                                                         /*1-conectado 0-nao conectado*/
+	info nome[255];
+	int indice;                                                         /*qualquer indice que o usuario quizer*/                                                    
+
+}MATRIZ;
+
+/*grafo estrutura grafo[linha][coluna]*/
+
+/*ex: na diagonal principal se o vertice esta conectado a si mesmo quer dizer que ele eh valido e que foi inicializado com um nome*/
+
 void cabecalho(int menu);    
 
 #endif
--- grafos/grafo_main.c
+++ grafos/grafo_main.c
@@ -6,63 +6,167 @@
 
 int main()
 {
-  
-  int vertices=0;
-  int sair=0;
-  int i=0,indice=0;
-  char entrada[255];
-  char string[255];
-      
-  do{
-    cabecalho(1); 
+   int vertices=0,vertices_ok=0;
+   int arestas_ok=0;
+   int code=0;
+   int i=0;	
+   char entrada[255];
+   char string[255];
+   int tamanho=0;
+ /*  int saida=0;
+   int chegada=0;
+   int peso=0;*/
+   int indice=0;
+   char nome[255];
+   int j=0;  															/*percorre string*/
+   char aux[255];
+   int k;
+   
+   do{
+	cabecalho(1);
     scanf("%s",entrada);
-    if((strcmp(entrada,"*Quit")==0)||(strcmp(entrada,"*quit")==0)){
-	   sair=1;  														/*sair do algoritmo*/
+	
+	if(strcmp(entrada,"*Quit")==0){ 
+	  printf("Algoritmo encerrado.\n");
+      exit(0);
+    }
+    if(strcmp(entrada,"*Vertices")==0){
+	   scanf("%d",&vertices);
+	   if(vertices_ok!=1){
+		 code=1;
+	   }
+	   else{  															/*mensagem de que o grafo ja tem vertices inicializados*/
+	      code=-2;
+	   }
 	}
-    else{  
-      if((strcmp(entrada,"*Vertices")==0)||(strcmp(entrada,"*vertices")==0)){
-        scanf("%d",&vertices);
-        i=1;
-        if(vertices>0){
-          scanf("%d",&indice);
-          
-          /*verificar se indice eh uma numero*/
-          
-          scanf("%s",string);
-          while((i<vertices)&&((strcmp(string,"*Arcs")!=0)&&(strcmp(string,"*arcs")!=0))&&((strcmp(string,"*Edges")!=0)&&(strcmp(string,"*edges")!=0))){
-          
-          
-            /*nao ta funcionando o while direito*/
-            /*testar para ver se ja existe nodo inicializado com o indice*/           
-          
-          
-            printf("indice - %d | Nome_Nodo - %s\n",indice,string);
-            scanf("%d",&indice);
-            scanf("%s",string);    
-            i++;    
-          }																/*fim while*/
-		  
-		} 																/*fim if sem vertices*/  
-		else{
-		   printf("Nenhum vertice a ser inicializado.\n");
-		}
-		printf("Fim da inicializacao dos vertices, inicio da inicializazao das arestas.\n");
-		/*comecar a tratar as arestas*/
-		sleep(2);   
-        printf("\e[H\e[2J");   
-        sair=0;
-      }
-      else{
-        printf("Comando inexistente.\n");
-        sleep(2);   
-        printf("\e[H\e[2J");   
-        sair=0;
-      }
-    }     
-    getchar();
-  }while(sair==0);
-  
-  printf("Algoritmo sendo encerrado.\n");
-  /*free(matriz);*/
-  return 0;
+    if(strcmp(entrada,"*Edges")==0){
+	   if(vertices_ok!=0){
+	     if(arestas_ok!=1){
+	       code=2;
+	     } 
+	     else{
+		   code=-2; 
+		 }
+	   }
+	   else{
+	     code=-1; 
+	   }  
+     }  
+    if(strcmp(entrada,"*Arcs")==0){
+	   if(vertices_ok!=0){
+	     if(arestas_ok!=1){
+	       code=3;
+	     } 
+	     else{
+		   code=-2; 
+		 }
+	   }
+	   else{
+	     code=-1; 
+	   }  
+	}
+	if(strcmp(entrada,"*Queries")==0){
+	  if(arestas_ok!=0){
+	    code=4;
+	  }	
+	  else{
+	    code=-3;
+	  }
+	}
+   
+   
+   
+   
+    switch(code){
+   
+      case -4:
+           printf("Arestas ja inicializadas.\n");  
+           break;
+      case -3:
+           printf("Arestas nao inicializadas. Digite '*Edges' ou '*Arcs' para mapear as Arestas.\n");  
+           break;
+      case -2:
+           if(arestas_ok!=1){
+             printf("Grafo ja inicializado. Digite '*Edges' ou '*Arcs' para mapear as Arestas.\n");  
+           }
+           else{
+		     printf("Grafo ja inicializado.Arestas ja inicializadas. Selecione uma Operacao a ser realizada no grafo.\n");
+		   }  
+           break;
+      case -1:
+           printf("Grafo nao inicializado. Digite inicialmente '*Vertices' e o numero de vertices do grafo.\n");  
+           break;
+      case 1:   														/* *Vertices*/
+           if(vertices>0){
+			  i=-1;
+			  getchar();
+			  do{
+			    i++;  													/*percorre todos o vertices do grafo*/
+			    j=-1;
+			    do{   					 								/*ateh dar enter*/  
+			      j++;
+			      scanf("%c",&string[j]);
+			    }while(string[j]!='\n');	
+			    string[j]='\0'; 										/*fim*/
+			    tamanho=strlen(string);
+			    if(string[0]!='*'){
+			      j=0;
+			      while(string[j]!=' '){
+				     aux[j]=string[j];
+				     j++;
+				  }
+				  aux[j]='\0';
+				  indice=atoi(aux);
+				  k=0;
+				  while(j<tamanho){
+				     aux[k]=string[j];
+				     j++;
+				     k++;
+				  }
+				  aux[k]='\0';
+				  strcpy(nome,aux);
+                } 
+			    /*tratar o nome*/
+			 }while((i<(vertices*vertices))&&(strcmp(string,"*Edges")!=0)&&(strcmp(string,"*Arcs")!=0));
+		    /* getchar();*/
+		     if(i>=(vertices*vertices)){
+		        printf("A Matriz de Adjacencia do grafo ja esta completa.\n");
+		     }
+		     vertices_ok=1;
+		   }
+		   else{
+		     printf("Erro.Numero minimo de 1 vertice nao atingido.\n");
+		   }
+           break;
+      case 2:															/* *Edges*/		
+           printf("*Edges.\n");
+           arestas_ok=1;
+           break;     
+     case 3:															/* *Arcs*/		
+           printf("*Arcs.\n");
+           arestas_ok=1;
+           break;     
+     case 4:															/* *Queries*/		
+           printf("*Queries.\n");
+           break;          
+     
+   
+      default:
+           printf("Instrucao nao implementada.\n");
+           code=5;  /*modificar no menu*/
+           break;
+       
+    }
+   sleep(2);
+   printf("\33[H\33[2J");
+   getchar();
+   }while(code!=0);
+
+
+
+
+
+
+
+   return 0;
 }
